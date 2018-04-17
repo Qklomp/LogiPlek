8e81fe9350d855ea2886222d37d1ee4697b7afc6
Jeroen Hoegen <Jeroenhoegen1998@gmail.com>
2018-04-17 08:27:22 +0200

    imp login secure

--- LogiPlekv2/application/models/Gebruiker.php
+++ LogiPlekv2/application/models/Gebruiker.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Jeroen Hoegen
- * Date: 16-4-2018
- * Time: 12:48
- */
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+Class User_model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_users()
+    {
+        $this->db->select('email');
+        $this->db->from('gebruiker');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+}
+?>
